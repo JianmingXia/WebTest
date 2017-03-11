b24db89253ffbcb895ac8902c6dae40fd190eaf5
Ryoma <xiajianming@quqi.com>
2017-03-11 21:50:26 +0800

    add WebTest

---
+++ test/index.js
@@ -0,0 +1,12 @@
+console.log("index.js");
+
+var arr = [1, 2, 3, 4, 5];
+
+for(let i = 0; i < arr.length; i++) {
+    // setTimeout(function(i){
+    //     console.log(i)
+    // }(i), 1000);
+    setTimeout(function(){
+        console.log(i)
+    }, 1000);
+}
